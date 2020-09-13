96a4d4178ad2ae734e23f98486f8e517eb4d4fec
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-09-13 16:30:36 +0200

    test

--- __manifest__.py
+++ __manifest__.py
@@ -22,6 +22,7 @@
     # any module necessary for this one to work correctly
     'depends': ['base','mail','helpdesk_inherit','report_xml'],
 
+
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
