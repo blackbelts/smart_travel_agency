95633466a849229343f6ddcb29775306c9ea7426
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-12-09 02:10:54 +0200

    update

--- wizard/users.py
+++ wizard/users.py
@@ -13,10 +13,16 @@ class AgentUsersWizard(models.TransientModel):
 
     # @api.multi
     def generate_users(self):
-        self.env['res.users'].create({'name': self.name, 'login': self.name,'password':self.password, 'groups_id': [
-            self.env['res.groups'].search([('name', '=', 'User: All Agency Documents')]).id]})
+        self.env['res.users'].create({'name': self.name, 'login': self.name,'password':self.password,
+                                      'sel_groups_1_8_9' : '1','in_group_17': True})
+            #                           'groups_id': [
+            # self.env['res.groups'].search([('name', '=', 'User: All Agency Documents')]).id]})
 
     # @api.multi
     def generate_branch_users(self):
-        self.env['res.users'].create({'name': self.name+'-'+str(self.travel_agency.agency_code), 'login': self.name+'-'+str(self.travel_agency.agency_code),'travel_agency_branch':self.id,'travel_agency':self.travel_agency.id, 'password': self.password, 'groups_id': [
-            self.env['res.groups'].search([('name', '=', 'User: Own Documents')]).id]})
+        self.env['res.users'].create({'name': self.name+'-'+str(self.travel_agency.agency_code),
+                                      'login': self.name+'-'+str(self.travel_agency.agency_code),
+                                      'travel_agency_branch':self.id,'travel_agency':self.travel_agency.id,
+                                      'password': self.password,'sel_groups_1_8_9' : '1','in_group_16': True})
+            #                           'groups_id': [
+            # self.env['res.groups'].search([('name', '=', 'User: Own Documents')]).id]})
