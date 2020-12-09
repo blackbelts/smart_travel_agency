54ed69c40f9aa42bf9c7bea1db76eed674441e5c
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-12-09 03:34:54 +0200

    update

--- wizard/users.py
+++ wizard/users.py
@@ -23,7 +23,7 @@ class AgentUsersWizard(models.TransientModel):
     def generate_branch_users(self):
         self.env['res.users'].create({'name': self.name+'-'+str(self.travel_agency.agency_code),
                                       'login': self.name+'-'+str(self.travel_agency.agency_code),
-                                      'travel_agency_branch':self.id,'travel_agency':self.travel_agency.id,
+                                      'travel_agency_branch':self.travel_agency_branch.id,'travel_agency':self.travel_agency.id,
                                       'password': self.password,'sel_groups_1_8_9' : '1','in_group_16': True})
             #                           'groups_id': [
             # self.env['res.groups'].search([('name', '=', 'User: Own Documents')]).id]})
