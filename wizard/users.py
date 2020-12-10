72f12fdaae33fa42f5075820faae6ebd8c1fd9ef
blackbelts-egypt <mabdelwahed120468@gmail.com>
2020-12-10 10:31:19 +0200

    update

--- wizard/users.py
+++ wizard/users.py
@@ -21,8 +21,8 @@ class AgentUsersWizard(models.TransientModel):
 
     # @api.multi
     def generate_branch_users(self):
-        self.env['res.users'].create({'name': self.name+'-'+str(self.travel_agency.agency_code),
-                                      'login': self.name+'-'+str(self.travel_agency.agency_code),
+        self.env['res.users'].create({'name': self.name,
+                                      'login': self.name,
                                       'travel_agency_branch':self.travel_agency_branch.id,'travel_agency':self.travel_agency.id,
                                       'password': self.password,'sel_groups_1_8_9' : '1','in_group_16': True})
             #                           'groups_id': [
